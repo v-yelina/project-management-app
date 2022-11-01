1b25b2914a5643aa1756e85426627171595922b3
sokolw <sokolw21@gmail.com>
2022-11-02 01:13:36 +0300

    feat: add comments for test

--- project-management-app/src/app/app.component.ts
+++ project-management-app/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+// this comments for test auth-interceptor, should delete
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent /* implements OnInit */ {
   title = 'project-management-app';
+
+/*  constructor(private store: Store, private usersApiService: UsersApiService) {
+  }
+
+  ngOnInit() {
+    this.usersApiService.getUsers().subscribe(console.log);
+  } */
 }
