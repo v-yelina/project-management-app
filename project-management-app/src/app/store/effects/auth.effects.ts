e9c31fe34fc418d608c5750452aade589740825d
sokolw <sokolw21@gmail.com>
2022-11-02 23:04:11 +0300

    feat: add signIn$, additionalUserData$ effects

--- project-management-app/src/app/store/effects/auth.effects.ts
+++ project-management-app/src/app/store/effects/auth.effects.ts
@@ -1,10 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { of, switchMap, tap } from 'rxjs';
-import { logOut, updateAuthState, updateAuthStateFromLocalStorage } from '../actions/auth.actions';
+import {
+  map, of, switchMap, tap, zip,
+} from 'rxjs';
+import { Store } from '@ngrx/store';
+import {
+  getAdditionalUserData,
+  logOut, signIn, updateAuthState, updateAuthStateFromLocalStorage,
+} from '../actions/auth.actions';
 import { LocalStorageService } from '../../core/services/local-storage.service';
 import { AUTH_STATE } from '../../core/constants/constants';
 import { initialState } from '../states/auth.state';
+import { AuthApiService } from '../../api/auth-api.service';
+import { UsersApiService } from '../../api/users-api.service';
+import { getAuthState } from '../selectors/auth.selectors';
+import { UserResponse } from '../../core/models/response-api.models';
 
 @Injectable()
 export class AuthEffects {
@@ -26,5 +36,40 @@ export class AuthEffects {
     }),
   ), { dispatch: false });
 
-  constructor(private actions$: Actions, private localStorageService: LocalStorageService) {}
+  signIn$ = createEffect(() => this.actions$.pipe(
+    ofType(signIn),
+    switchMap((action) => zip(of(action), this.authApiService.signIn(action.payload))),
+    map(([action, response]) => updateAuthState({
+      payload: {
+        token: response.token,
+        name: null,
+        login: action.payload.login,
+        id: null,
+      },
+    })),
+  ));
+
+  additionalUserData$ = createEffect(() => this.actions$.pipe(
+    ofType(getAdditionalUserData),
+    switchMap(() => zip(this.usersApiService.getUsers(), this.store.select(getAuthState))),
+    map(([users, state]) => {
+      const user = users.find((item) => item.login === state.login) as UserResponse;
+      return updateAuthState({
+        payload: {
+          ...state,
+          name: user.name,
+          // eslint-disable-next-line no-underscore-dangle
+          id: user._id,
+        },
+      });
+    }),
+  ));
+
+  constructor(
+    private actions$: Actions,
+    private localStorageService: LocalStorageService,
+    private authApiService: AuthApiService,
+    private usersApiService: UsersApiService,
+    private store: Store,
+  ) {}
 }
