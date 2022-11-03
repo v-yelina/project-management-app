956335de98b19a5c33f456347bec28ef2410cc08
v-yelina <heytridvaodin@gmail.com>
2022-11-03 17:11:38 +0100

    fix: change loading type for welcome page

--- project-management-app/src/app/app-routing.module.ts
+++ project-management-app/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NotFoundPageComponent } from './core/pages/not-found-page/not-found-page.component';
+import { WelcomePageComponent } from './core/pages/welcome-page/welcome-page.component';
 
 const routes: Routes = [
   { path: 'auth', loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule) },
-  { path: 'welcome', loadChildren: () => import('./core/core.module').then((m) => m.CoreModule) },
+  { path: 'welcome', component: WelcomePageComponent },
   { path: 'boards', loadChildren: () => import('./board/board.module').then((m) => m.BoardModule) },
   { path: '', redirectTo: '/boards', pathMatch: 'full' },
   { path: '**', component: NotFoundPageComponent },
