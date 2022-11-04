bf76ea001fa2846694566f31689bf0049fe476cd
v-yelina <heytridvaodin@gmail.com>
2022-11-04 12:42:49 +0100

    feat: add confirm dialog

--- project-management-app/src/app/shared/components/confirm-popup/confirm-popup.component.ts
+++ project-management-app/src/app/shared/components/confirm-popup/confirm-popup.component.ts
@@ -1,13 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ConfirmData } from './confirm-popup.models';
 
 @Component({
   selector: 'app-confirm-popup',
   templateUrl: './confirm-popup.component.html',
   styleUrls: ['./confirm-popup.component.scss'],
 })
-export class ConfirmPopupComponent implements OnInit {
-  constructor() { }
 
-  ngOnInit(): void {
+export class ConfirmPopupComponent {
+  confirmMessage = "Are you sure?"
+  confirmDeleteMessage = "Are you sure you want to delete this"
+  confirmButtonText = "Confirm"
+  cancelButtonText = "Cancel"
+  constructor(@Inject(MAT_DIALOG_DATA) private data: ConfirmData, private dialogRef: MatDialogRef<ConfirmPopupComponent>) {
+    if (this.data) {
+      this.confirmMessage = this.data.message
+        || `${this.confirmDeleteMessage} ${this.data.itemType}?`
+    }
+  }
+
+  onConfirmClick(): void {
+    this.dialogRef.close(true);
   }
 }
