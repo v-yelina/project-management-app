28192def5fda1e68691d2b5e837cdb95712abfb7
sokolw <sokolw21@gmail.com>
2022-11-09 17:39:23 +0300

    feat: add InfoPopupComponent, MatInputModule, ReactiveFormsModule, MatButtonModule

--- project-management-app/src/app/shared/shared.module.ts
+++ project-management-app/src/app/shared/shared.module.ts
@@ -3,11 +3,20 @@ import { CommonModule } from '@angular/common';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ConfirmPopupComponent } from './components/confirm-popup/confirm-popup.component';
+import { InfoPopupComponent } from './components/info-popup/info-popup.component';
 
 @NgModule({
-  declarations: [ConfirmPopupComponent],
+  declarations: [ConfirmPopupComponent, InfoPopupComponent],
   imports: [CommonModule, MatDialogModule, MatCardModule, MatButtonModule],
-  exports: [ConfirmPopupComponent],
+  exports: [
+    ConfirmPopupComponent,
+    InfoPopupComponent,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
 })
 export class SharedModule {}
