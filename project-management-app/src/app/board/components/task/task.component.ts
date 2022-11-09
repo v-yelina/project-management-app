c23b75127e98da3dcdcda2580ee131bf47c1eb67
v-yelina <heytridvaodin@gmail.com>
2022-11-09 14:25:36 +0100

    feat: add onclick logic to delete task button

--- project-management-app/src/app/board/components/task/task.component.ts
+++ project-management-app/src/app/board/components/task/task.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
 import { TaskResponse } from 'src/app/core/models/response-api.models';
+import { ConfirmPopupComponent } from 'src/app/shared/components/confirm-popup/confirm-popup.component';
+import { ItemType } from 'src/app/shared/components/confirm-popup/confirm-popup.models';
 
 @Component({
   selector: 'app-task',
@@ -7,9 +10,31 @@ import { TaskResponse } from 'src/app/core/models/response-api.models';
   styleUrls: ['./task.component.scss'],
 })
 export class TaskComponent {
-  taskData: Pick<TaskResponse, 'description' | 'title'> = {
+  taskData: Pick<TaskResponse, 'description' | 'title' | '_id'> = {
     title: 'New Task Title',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et',
+    _id: 'sshdh1jsk8767'
   };
+
+  constructor(private dialog: MatDialog) { }
+
+  openDialog(id: string) {
+    const dialogRef = this.dialog.open(ConfirmPopupComponent, {
+      data: {
+        itemType: ItemType.task,
+      }
+    });
+
+    dialogRef.afterClosed().subscribe((confirmed: boolean) => {
+      if (confirmed) {
+        this.deleteTask(id);
+      }
+    });
+  }
+
+  deleteTask(id: string) {
+    console.log(`delete task ${id}`);
+  }
+
 }
