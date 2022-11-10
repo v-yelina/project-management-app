9786a88e1101cffa4fb1f159efda4b497b5cf4ea
v-yelina <heytridvaodin@gmail.com>
2022-11-10 12:03:52 +0100

    feat: add edit column title form

--- project-management-app/src/app/board/components/column/column.component.ts
+++ project-management-app/src/app/board/components/column/column.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-column',
   templateUrl: './column.component.html',
   styleUrls: ['./column.component.scss']
 })
-export class ColumnComponent implements OnInit {
+export class ColumnComponent {
+
+  title = 'Column title';
+  editMode = false;
+
+  editTitleForm: FormGroup = new FormGroup({
+    columnTitle: new FormControl(this.title, [Validators.required]),
+  });
 
   constructor() { }
 
