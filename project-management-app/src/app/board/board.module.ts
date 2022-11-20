d86f392a3bc8bc01a1c3efbd8e40a0c3eaa514d9
v-yelina <heytridvaodin@gmail.com>
2022-11-20 15:02:00 +0100

    fix: boards page route

--- project-management-app/src/app/board/board.module.ts
+++ project-management-app/src/app/board/board.module.ts
@@ -19,7 +19,7 @@ import { BoardComponent } from './pages/board/board.component';
 import { CreateColumnComponent } from './components/create-column/create-column.component';
 
 const routes: Routes = [
-  { path: 'board', component: BoardComponent },
+  { path: '', component: BoardsPageComponent },
   { path: ':id', component: BoardComponent },
 ];
 
@@ -50,4 +50,4 @@ const routes: Routes = [
   ],
   exports: [BoardComponent],
 })
-export class BoardModule {}
+export class BoardModule { }
