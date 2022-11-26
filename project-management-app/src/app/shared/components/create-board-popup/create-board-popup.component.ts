29c8fe0f100283ef44862c3cfc152d6ad6b41c0f
sokolw <sokolw21@gmail.com>
2022-11-26 13:39:52 +0300

    feat: add check invited user is exist

--- project-management-app/src/app/shared/components/create-board-popup/create-board-popup.component.ts
+++ project-management-app/src/app/shared/components/create-board-popup/create-board-popup.component.ts
@@ -1,22 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatChipInputEvent } from '@angular/material/chips';
 import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Subscription } from 'rxjs';
+import { Store } from '@ngrx/store';
 import { RawBoard } from '../../../core/models/board.models';
 import { RestApiService } from '../../../core/services/rest-api.service';
+import { UserResponse } from '../../../core/models/response-api.models';
+import { loaded } from '../../../store/actions/notifications.actions';
 
 @Component({
   selector: 'app-create-board-popup',
   templateUrl: './create-board-popup.component.html',
   styleUrls: ['./create-board-popup.component.scss'],
 })
-export class CreateBoardPopupComponent {
+export class CreateBoardPopupComponent implements OnDestroy {
   addOnBlur = true;
 
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
 
   members: Array<string> = [];
+
+  users: Array<UserResponse> = [];
+
+  subscription = new Subscription();
+
+  userStatus = { exist: true, name: '' };
 
   boardForm = new FormGroup({
     name: new FormControl('', [Validators.required]),
@@ -25,7 +35,12 @@ export class CreateBoardPopupComponent {
   constructor(
     public dialogRef: MatDialogRef<CreateBoardPopupComponent>,
     private restApiService: RestApiService,
+    private store: Store,
   ) {}
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 
   onNoClick(): void {
     this.dialogRef.close();
@@ -35,7 +50,18 @@ export class CreateBoardPopupComponent {
     const value = (event.value || '').trim();
 
     if (value) {
-      this.members.push(value);
+      if (this.users.length === 0) {
+        const subUsers = this.restApiService.getUsers().subscribe((users) => {
+          this.users = users;
+          this.store.dispatch(loaded());
+          this.checkUser(value);
+          this.addToMembers(value);
+        });
+        this.subscription.add(subUsers);
+      } else {
+        this.checkUser(value);
+        this.addToMembers(value);
+      }
     }
 
     event.chipInput!.clear();
@@ -51,5 +77,18 @@ export class CreateBoardPopupComponent {
 
   getInputResults(): RawBoard {
     return { name: this.boardForm.get('name')?.value as string, members: this.members };
+  }
+
+  private checkUser(userLogin: string): void {
+    this.userStatus = {
+      exist: !!this.users.find((user) => user.login === userLogin),
+      name: userLogin,
+    };
+  }
+
+  private addToMembers(userLogin: string): void {
+    if (this.userStatus.exist) {
+      this.members.push(userLogin);
+    }
   }
 }
